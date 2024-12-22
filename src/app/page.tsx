91282cd133713714573dcdcee76126bb2511bddd
iqra-ikram <iqraikram58@gmail.com>
2024-12-22 13:08:52 +0500

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,57 @@
-import Image from "next/image";
 
-export default function Home() {
+const page = () => {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    <>
 
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+    <h1 className="text-blue-700 font-extrabold text-2xl justify-center items-center pl-11 mt-7 bg-yellow-50 ">Class 09 Assignment</h1>
+    <p className="mt-9"> 1. Make Card Layout of Governor Sindh Website:</p>
+   <p> 2. In first row there should be 2 cards using Flexbox.</p>
+   <p> 3.In second row there should be 3 cards using Flexbox.</p>
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+  
+<h1 className="text-blue-900 text-[27px] font-extrabold pt-[50px] text-center">Core Courses Sequence</h1>
+<div className="md:flex text-center">
+<div className=" hover:scale-105">
+  <img className="md:h-[260px] md:w-[400px] h-[260px] w-[350px] ml-[40px] pt-[50px] rounded-xl" src="https://www.governorsindh.com/_next/static/media/programming_fundamentals.49cca4e9.jpg" alt="" />
+<p className=" font-bold text-[20px] text-gray-500 pt-3">Programing Fundamentals</p>
+</div>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+<div className=" hover:scale-105">
+  <img className="md:h-[260px] md:w-[400px] h-[260px] w-[350px] ml-[40px] pt-[50px] rounded-xl" src="https://www.governorsindh.com/_next/static/media/nextjs.3dff0f70.jpg" alt="" />
+<p className=" font-bold text-[20px] text-gray-500 pt-3"> Web2 Using NextJS</p>
+</div>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  );
+</div>
+
+<h1 className="text-blue-900 text-[27px] font-extrabold pt-[50px] text-center">Advanced Courses</h1>
+
+<div className="md:flex text-center">
+
+<div className=" hover:scale-105">
+  <img className=" md:h-[250px] md:w-[300px] h-[260px] w-[350px] ml-[40px] pt-[50px] rounded-xl" src="https://www.governorsindh.com/_next/static/media/AI.39397d24.jpg" alt="" />
+<p className=" font-bold text-[20px] text-gray-500 pt-3">Artifical intelligence</p>
+</div>
+
+<div className=" hover:scale-105">
+  <img className=" md:h-[250px] md:w-[300px]  h-[260px] w-[350px] ml-[40px] pt-[50px] rounded-xl" src="https://www.governorsindh.com/_next/static/media/metaverse.06eccb60.jpg" alt="" />
+<p className=" font-bold text-[20px] text-gray-500 pt-3"> Web 3 and Metaverse</p>
+</div>
+
+<div className=" hover:scale-105">
+  <img className="md:h-[250px] md:w-[300px]  h-[260px] w-[350px] ml-[40px] pt-[50px] rounded-xl" src="https://www.governorsindh.com/_next/static/media/cloudComputing.7260492c.jpg" alt="" />
+<p className=" font-bold text-[20px] text-gray-500 pt-3">Cloud-Native Computing</p>
+</div>
+
+
+
+</div>
+
+
+
+
+    </>
+  )
 }
+
+export default page 
